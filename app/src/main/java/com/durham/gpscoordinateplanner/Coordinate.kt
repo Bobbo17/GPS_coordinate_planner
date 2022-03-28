61476566e9c647692aa87a6afaa3ffab203e81eb
Bobbo17 <rdurham17@gmail.com>
2022-03-27 18:05:09 -0700

    added ability to save text file with gps coordinates in comma separated format (tried with comma separated file but had issues opening on android device) changed the coordinate list to only update after new coordinates are registered.

---
+++ app/src/main/java/com/durham/gpscoordinateplanner/Coordinate.kt
@@ -0,0 +1,8 @@
+package com.durham.gpscoordinateplanner
+
+import android.location.Location
+
+class Coordinate (name:String,location:Location) {
+    val Loc = location
+    val Name = name
+}
--- app/src/main/java/com/durham/gpscoordinateplanner/MainActivity.kt
+++ app/src/main/java/com/durham/gpscoordinateplanner/MainActivity.kt
@@ -1,24 +1,30 @@
 package com.durham.gpscoordinateplanner
 
 import android.Manifest
-import android.app.Service
 import android.content.Context
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.DocumentsContract
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationServices
-import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Tasks.await
+import com.google.android.material.snackbar.Snackbar
+import java.io.File
+import java.lang.Exception
 import kotlin.system.exitProcess
 
 class MainActivity : AppCompatActivity() {
@@ -26,9 +32,11 @@ class MainActivity : AppCompatActivity() {
     lateinit var btnLocation: Button
     lateinit var btnExport:Button
     lateinit var tvList:TextView
+    lateinit var ebEntranceInfo:EditText
     private var locationGps : Location? = null
     private var locationNetwork : Location? = null
     private var locationList:ArrayList<Location> = ArrayList<Location>()
+    private var coordinateList:ArrayList<Coordinate> = ArrayList<Coordinate>()
 
     var hasGPS:Boolean = false
     var hasNetwork:Boolean = false
@@ -59,6 +67,7 @@ class MainActivity : AppCompatActivity() {
 //        btnLocation = findViewById(R.id.btnAddRecord)
 //        btnExport = findViewById(R.id.btnExportList)
         tvList = findViewById(R.id.tvLocationList)
+        ebEntranceInfo = findViewById(R.id.tbEditText)
 
 
 
@@ -66,8 +75,30 @@ class MainActivity : AppCompatActivity() {
 
         flpc = LocationServices.getFusedLocationProviderClient(this)
 
+    }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
 
+        //BobCode
+        var saveData = "Name, latitude, longitude, altitude, bearing, bearingAccuracyDegrees, accuracy, verticalAccuracy\n"
+        for( i in coordinateList){
+            saveData += i.Name + ", " + i.Loc.latitude + ", " + i.Loc.longitude + ", " + i.Loc.altitude + ", " + i.Loc.bearing + ", " +
+            i.Loc.bearingAccuracyDegrees + ", " + i.Loc.accuracy + ", " + i.Loc.verticalAccuracyMeters + "\n"
+        }
+
+        if (requestCode == CREATE_FILE && resultCode == RESULT_OK){
+            val uri = data!!.data
+            try{
+                val outputStream = this.contentResolver.openOutputStream(uri!!)
+                outputStream?.write(saveData.toByteArray())
+                outputStream?.close()
+                Toast.makeText(this, "coordinate list saved",Toast.LENGTH_LONG).show()
+            }catch(e:Exception){
+                print(e.localizedMessage)
+                Toast.makeText(this, "Error While Saving File",Toast.LENGTH_LONG).show()
+            }
+        }
     }
 
     //launches the permission request for the specific record_audio permission required
@@ -122,6 +153,8 @@ class MainActivity : AppCompatActivity() {
                     if (location!=null) {
                         locationGps = location
                         locationList.add(location)
+                        coordinateList.add(Coordinate(ebEntranceInfo.text.toString(), location))
+                        updateText()
                     }
                 }
 
@@ -196,28 +229,62 @@ class MainActivity : AppCompatActivity() {
     fun AddRecord() {
 
     }
-
+    private fun updateText(){
+        var ogs:StringBuilder = StringBuilder()
+        for (l in coordinateList.reversed()) {
+            ogs.append(
+                "location: " + l.Name + "\nalt: " + l.Loc.altitude + "\nlat: " + l.Loc.latitude + "\nlong: " + l.Loc.longitude +
+                        "\naccuracy: " + l.Loc.accuracy + "\n\n"
+            )
+            Log.d(
+                "GPS SUCCESS",
+                "Stringbuilder has appended the altitude, latitude, longitude, and accuracy"
+            )
+        }
+        tvList.text = ogs.toString()
+    }
     //Buttons
 
     fun onContinueClick(v: View){
         checkPermissions()
     }
 
-    fun onAddRecordClick(v:View){
-        getLocation()
-        var displayText:String = ""
-        if (locationList.isNotEmpty()) {
-            var ogs:StringBuilder = StringBuilder()
-            for (l in locationList){
-                ogs.append("alt: " + l.altitude + "\nlat: " + l.latitude + "\nlong: " + l.longitude +
-                        "\naccuracy: " + l.accuracy + "\n")
-                Log.d("GPS SUCCESS", "Stringbuilder has appended the altitude, latitude, longitude, and accuracy")
-            }
-            displayText = ogs.toString()
-        } else{
-            
+    fun onExportClick(v:View){
+        //https://developer.android.com/training/data-storage/shared/documents-files
+        //text/csv
+        createFile()
+        //Snackbar.make(v, this.getFilesDir().toString(), Snackbar.LENGTH_LONG).show()
+    }
 
+    fun onEditTextClick(v:View){
+        if (ebEntranceInfo.text.toString() == "Location Info"){
+            ebEntranceInfo.setText("")
+            Snackbar.make(v, "blanking text... not!", Snackbar.LENGTH_LONG).show()
         }
+    }
+    // Request code for creating a PDF document.
+    val CREATE_FILE = 0
+
+    private fun createFile() {
+        val intent = Intent(Intent.ACTION_CREATE_DOCUMENT).apply {
+            addCategory(Intent.CATEGORY_OPENABLE)
+            type = "text/plain"
+            putExtra(Intent.EXTRA_TITLE, "coordinates.txt")
+
+            // Optionally, specify a URI for the directory that should be opened in
+            // the system file picker before your app creates the document.
+            putExtra(DocumentsContract.EXTRA_INITIAL_URI, "/CollegeProjectPTK2022")
+        }
+        startActivityForResult(intent, CREATE_FILE)
+    }
+
+    fun onAddRecordClick(v:View){
+        if(ebEntranceInfo.text.toString() != "" && ebEntranceInfo.text.toString() != "Location Info"){
+            getLocation()
+        }else{
+            Snackbar.make(v, "must have building info in the text input box", Snackbar.LENGTH_LONG).show()
+        }
+
 //        val l = locationGps
 //        var ogs:String = tvList.text.toString()
 //        if (l != null){
@@ -227,7 +294,7 @@ class MainActivity : AppCompatActivity() {
 //        }else {
 //            ogs += "\nlocation value was null"
 //        }
-        tvList.text = displayText
+
     }
 
 }
